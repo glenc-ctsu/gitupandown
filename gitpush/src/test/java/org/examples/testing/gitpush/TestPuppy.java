728da4412829efe483b848de2eaa37e791e80f2e
glen ctsu <guanguo.cui@ctsu.ox.ac.uk>
2016-03-17 13:23:42 +0000

    v4

---
+++ gitpush/src/test/java/org/examples/testing/gitpush/TestPuppy.java
@@ -0,0 +1,11 @@
+package org.examples.testing.gitpush;
+
+import org.testng.annotations.Test;
+
+public class TestPuppy {
+	
+	@Test
+	public void testMethod(){
+		System.out.println("aaaaaaaaaaaa");
+	}
+}
